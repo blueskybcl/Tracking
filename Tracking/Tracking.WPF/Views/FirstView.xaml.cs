b4a92aca119d39bd3d596f613da5d9ed63afa8f1
baichonglong <a364711322@sina.com>
2017-11-25 00:08:56 +0800

    Add project files.

---
+++ Tracking/Tracking.WPF/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using MvvmCross.Wpf.Views;
+
+namespace Tracking.WPF.Views
+{
+    public partial class FirstView : MvxWpfView
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
