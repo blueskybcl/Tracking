0630b9d5984b7d34e2708c6d8ae93dc8ecb7de55
baichonglong <a364711322@sina.com>
2017-12-08 14:40:07 +0800

    add auto nav map control.

---
+++ Tracking/Tracking.Droid/Renderers/AutoNavMapRenderer.cs
@@ -0,0 +1,12 @@
+﻿
+using Tracking.Core.Controls;
+using Tracking.Droid.Renderers;
+using Xamarin.Forms;
+
+[assembly:ExportRenderer(typeof(AutoNavMap),typeof(AutoNavMapRenderer))]
+namespace Tracking.Droid.Renderers
+{
+    public class AutoNavMapRenderer
+    {
+    }
+}
