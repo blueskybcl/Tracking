48a28d2ca2ae5c3f88a83f02a6ee18d212898108
baichonglong <a364711322@sina.com>
2017-12-20 12:33:41 +0800

    Add iOS renderer for esri map.

---
+++ EsriMapPCLDemo/EsriMapPCLDemo.iOS/Renderer/Adapters/MapViewAdapter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Esri.ArcGISRuntime.Mapping;
+using EsriMapPCLDemo.Controls;
+using CusMapView = EsriMapPCLDemo.Controls.MapView;
+using EsriMapView = Esri.ArcGISRuntime.UI.Controls.MapView;
+using Map = Esri.ArcGISRuntime.Mapping.Map;
+
+namespace EsriMapPCLDemo.iOS.Renderer.Adapters
+{
+    public sealed class MapViewAdapter
+    {
+        private MapViewAdapter()
+        {
+        }
+
+        public static readonly MapViewAdapter Instance = new MapViewAdapter();
+
+        public EsriMapView Adapter(CusMapView cusMapView)
+        {
+            EsriMapView XFMapView = new EsriMapView();
+            MappingMap(XFMapView, cusMapView);
+            //TODO: Attach custom property
+            return XFMapView;
+        }
+
+        private void MappingMap(EsriMapView xfMapView, CusMapView cusMapView)
+        {
+            Basemap baseMap = GetBaseMap(cusMapView.Map.MapType);
+            xfMapView.Map = new Map(baseMap);
+        }
+
+        private Basemap GetBaseMap(MapType mapType)
+        {
+            switch (mapType)
+            {
+                case MapType.Imagery:
+                    return Basemap.CreateImagery();
+
+                case MapType.Streets:
+                    return Basemap.CreateStreets();
+
+                case MapType.ImageryWithLabels:
+                    break;
+
+                case MapType.ImageryWithLabelsVector:
+                    break;
+
+                case MapType.LightGrayCanvas:
+                    break;
+
+                case MapType.LightGrayCanvasVector:
+                    break;
+
+                case MapType.DarkGrayCanvasVector:
+                    break;
+
+                case MapType.NationalGeographic:
+                    break;
+
+                case MapType.Oceans:
+                    break;
+
+                case MapType.StreetsVector:
+                    break;
+
+                case MapType.StreetsWithReliefVector:
+                    break;
+
+                case MapType.StreetsNightVector:
+                    break;
+
+                case MapType.NavigationVector:
+                    break;
+
+                case MapType.TerrainWithLabels:
+                    break;
+
+                case MapType.TerrainWithLabelsVector:
+                    break;
+
+                case MapType.Topographic:
+                    break;
+
+                case MapType.TopographicVector:
+                    break;
+
+                case MapType.OpenStreetMap:
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mapType), mapType, null);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ EsriMapPCLDemo/EsriMapPCLDemo.iOS/Renderer/MapViewRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using Esri.ArcGISRuntime.UI.Controls;
+using EsriMapPCLDemo.iOS.Renderer;
+using EsriMapPCLDemo.iOS.Renderer.Adapters;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using CusMapView = EsriMapPCLDemo.Controls.MapView;
+
+[assembly: ExportRenderer(typeof(CusMapView), typeof(MapViewRenderer))]
+namespace EsriMapPCLDemo.iOS.Renderer
+{
+    public class MapViewRenderer : ViewRenderer<CusMapView, MapView>
+    {
+        private MapView OriginMapView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CusMapView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null)
+            {
+
+            }
+
+            CusMapView currentElement = e.NewElement;
+            if (currentElement != null)
+            {
+                OriginMapView = MapViewAdapter.Instance.Adapter(currentElement);
+            }
+
+            if (Control == null)
+            {
+                SetNativeControl(OriginMapView);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
